ecb138a4324f19672fc352b7a59c63edb1e2886d
montaser34 <mntsr9999.z@gmail.com>
2024-09-13 20:18:35 +0300

    fix bug and redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Button from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Header from "@/components/ui/Header";
 import { useState } from "react";
 
